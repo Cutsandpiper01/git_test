16e939c5aff5541df9bd733d4990e7ed04ac8064
Kalen Muller <kalenchandler@gmail.com>
2021-07-17 17:40:08 -0600

    UI addition to RPS

--- rps.js
+++ rps.js
@@ -11,11 +11,15 @@ function didPlayerWin(player){
 	console.log(compNum)
 	console.log(playerNum)
 	if(playerNum - compNum == 0){
+		interpretResult(0)
 		return 0
+
 	}else if(playerNum - compNum == -1 || playerNum-compNum == 2){
 		console.log(playerNum-compNum)
+		interpretResult(1)
 		return 1
 	}else{
+		interpretResult(-1)
 		return -1
 	}
 }
@@ -31,6 +35,7 @@ function inputToNumber(input){
 	}
 
 }
+
 
 function game(){
 	let playerscore = 0;
@@ -52,4 +57,58 @@ function game(){
 	}
 }
 
-game()
+function interpretResult(tempscore){
+	if(tempscore == 0){
+			divUpdate("Tie")
+		}else if(tempscore ==1 ){
+			divUpdate("Player won round!")
+			scoreUpdate(true)
+
+		}else{
+			//compscore += 1;
+			
+			divUpdate("Computer won round ):")
+			scoreUpdate(false)
+		}
+}
+
+function divUpdate(text){
+	const parent = document.querySelector('#container')
+
+	parent.textContent = text
+
+
+}
+
+function scoreUpdate(isWin){
+	const score = document.querySelector('#score')
+	if(isWin){
+		playerscore += 1
+		score.textContent = playerscore + " - " + compscore
+
+	}else{
+		compscore += 1
+		score.textContent = playerscore + " - " + compscore
+
+	}
+	if(playerscore == 5){
+		divUpdate("Player WINS!!")
+	}
+	if(compscore == 5){
+		divUpdate("Computer WINS!!")
+	}
+
+}
+let playerscore = 0;
+let compscore = 0;
+
+
+const rockB = document.querySelector('#rock')
+const paperB = document.querySelector('#paper')
+const scissorB = document.querySelector('#scissor')
+
+var result = 0
+rockB.addEventListener('click',function(e){result = didPlayerWin('rock')})
+paperB.addEventListener("click", function(e) {result = didPlayerWin('paper')})
+scissorB.addEventListener("click", function(e){result = didPlayerWin('scissor')})
+
